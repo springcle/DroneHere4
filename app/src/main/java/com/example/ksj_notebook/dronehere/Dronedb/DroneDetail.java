1d2bc6a20666f605920cb2b1f6d08d22f48d3e54
springcle <skrdbsdl1@gmail.com>
2017-03-17 17:27:33 +0900

    [2017-03-17(금) 17:26] - 드론 상세 페이지 완료 - 드론 리뷰 작성 페이지 완료 - 에디트 버튼 이미지 수정

--- app/src/main/java/com/example/ksj_notebook/dronehere/Dronedb/DroneDetail.java
+++ app/src/main/java/com/example/ksj_notebook/dronehere/Dronedb/DroneDetail.java
@@ -1,11 +1,14 @@
 package com.example.ksj_notebook.dronehere.Dronedb;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -43,6 +46,12 @@ public class DroneDetail extends BaseActivity implements ViewPager.OnPageChangeL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drone_detail);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.status2));
+        }
         dtAdapter=new DroneDetailAdapter();
         recyclerView=(RecyclerView)findViewById(R.id.dbdetailrecy);
         dt_float=(ImageButton)findViewById(R.id.dtdt);
